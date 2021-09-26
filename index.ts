5276d82f0ff17da305d06d62f253b67a0ec7217f
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-09-26 11:24:34 +0900

    8. TypeScriptのLinterとしての側面を理解する

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 let hello: string = 'hello';
 console.log(hello);
 
-function add(a: number, b: number): number {
+function add(a: string, b: number): number {
   return a + b;
 }
