542e1ee5ebf48a9d7a52eb2ad528b96a53d96b83
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-10-02 12:05:40 +0900

    22. Literal型を使って特定の値のみを取り扱う方法

--- TYPESCRIPT-TYPE/index.ts
+++ TYPESCRIPT-TYPE/index.ts
@@ -3,7 +3,7 @@ let count = 10;
 let float = 3.14;
 let negative = -0.12;
 let single = 'hello';
-let double = "hello";
+let double = 'hello';
 let back = `hello`;
 let hello: string;
 hello = 'hello';
@@ -43,3 +43,13 @@ banana = anything;
 
 let unionType: number | string = 10;
 let unionTypes: (number | string)[] = [21, 'hello'];
+
+const apple = 'apple';
+let clothSize: 'small' | 'medium' | 'large' = 'large';
+const cloth: {
+  color: string;
+  size: 'small' | 'medium' | 'large';
+} = {
+  color: 'white',
+  size: 'medium',
+};
