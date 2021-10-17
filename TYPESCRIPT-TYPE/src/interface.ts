b68effc42ec8374d9e52086fe7abf0cf6d39b700
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-10-17 11:39:01 +0900

    63. メソッドをオブジェクトの型に指定する方法

--- TYPESCRIPT-TYPE/src/interface.ts
+++ TYPESCRIPT-TYPE/src/interface.ts
@@ -1,10 +1,13 @@
 interface Human {
   name: string;
   age: number;
+  greeting(message: string): void;
 }
 
 const human: Human = {
   name: 'Quill',
   age: 38,
+  greeting(message: string) {
+    console.log(message);
+  },
 };
-let developer: Human;
