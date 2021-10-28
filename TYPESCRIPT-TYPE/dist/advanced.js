50454a0734bddcc71438b9e0399cd7621cc6618e
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-10-29 08:51:42 +0900

    84. 関数型のオーバーロードはinterfaceで定義する必要がある

--- TYPESCRIPT-TYPE/dist/advanced.js
+++ TYPESCRIPT-TYPE/dist/advanced.js
@@ -11,7 +11,9 @@ function toUpperCase(x) {
     }
     return x;
 }
-const upperHello = toUpperCase('hello');
+const upperHello = function (x) {
+    return 0;
+};
 function describeProfile(nomadWorker) {
     console.log(nomadWorker.name);
     if ('role' in nomadWorker) {
--- TYPESCRIPT-TYPE/src/advanced.ts
+++ TYPESCRIPT-TYPE/src/advanced.ts
@@ -26,7 +26,13 @@ function toUpperCase(x: string | number) {
   }
   return x;
 }
-const upperHello = toUpperCase('hello');
+interface TmpFunc {
+  (x: string): number;
+  (x: number): number;
+}
+const upperHello: TmpFunc = function (x: string | number) {
+  return 0;
+};
 
 type NomadWorker = Engineer | Blogger;
 function describeProfile(nomadWorker: NomadWorker) {
