27991033d986d275a7575ad12e701f2be5e6897c
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-11-02 08:07:32 +0900

    96. Classに対してジェネリクスを使用する方法

--- TYPESCRIPT-TYPE/dist/generics.js
+++ TYPESCRIPT-TYPE/dist/generics.js
@@ -4,3 +4,23 @@ function copy(value, key) {
     return value;
 }
 console.log(copy({ name: 'Quill' }, 'name'));
+class LightDatabase {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LightDatabase();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase.get());
--- TYPESCRIPT-TYPE/src/generics.ts
+++ TYPESCRIPT-TYPE/src/generics.ts
@@ -3,3 +3,22 @@ function copy<T extends {name: string}, U extends keyof T>(value: T, key: U): T 
   return value;
 }
 console.log(copy({name: 'Quill'}, 'name'));
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get() {
+    return this.data;
+  }
+}
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase.get());
