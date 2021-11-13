3a00fc8aa17d5cdea93e1c0bda7f2bb678967fd0
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-11-13 09:01:18 +0900

    125. アプリでシングルトンパターンを使用する

--- TYPESCRIPT-TYPE/src/food-app.ts
+++ TYPESCRIPT-TYPE/src/food-app.ts
@@ -1,10 +1,18 @@
 class Score {
+  private static instance: Score;
   get totalScore() {
-    const foods = new Foods();
+    const foods = Foods.getInstance();
     return foods.activeElementsScore.reduce((total, score) => total + score, 0);
   }
   render() {
     document.querySelector('.score__number')!.textContent = String(this.totalScore);
+  }
+  private constructor() {}
+  static getInstance() {
+    if (!Score.instance) {
+      Score.instance = new Score();
+    }
+    return Score.instance;
   }
 }
 class Food {
@@ -13,11 +21,12 @@ class Food {
   }
   clickEventHandler() {
     this.element.classList.toggle('food--active');
-    const score = new Score();
+    const score = Score.getInstance();
     score.render();
   }
 }
 class Foods {
+  private static instance: Foods;
   elements = document.querySelectorAll<HTMLDivElement>('.food');
   private _activeElements: HTMLDivElement[] = [];
   private _activeElementsScore: number[] = [];
@@ -40,10 +49,16 @@ class Foods {
     });
     return this._activeElementsScore;
   }
-  constructor() {
+  private constructor() {
     this.elements.forEach((element) => {
       new Food(element);
     });
   }
+  static getInstance() {
+    if (!Foods.instance) {
+      Foods.instance = new Foods();
+    }
+    return Foods.instance;
+  }
 }
-const foods = new Foods();
+const foods = Foods.getInstance();
