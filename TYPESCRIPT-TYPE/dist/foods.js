4cf94ea041f44d47cfa7e73ce3eadc1db6844549
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-11-14 13:19:17 +0900

    129. scriptタグを並べて、ファイルを分割する方法

--- TYPESCRIPT-TYPE/dist/foods.js
+++ TYPESCRIPT-TYPE/dist/foods.js
@@ -1,31 +1,4 @@
 "use strict";
-class Score {
-    constructor() { }
-    get totalScore() {
-        const foods = Foods.getInstance();
-        return foods.activeElementsScore.reduce((total, score) => total + score, 0);
-    }
-    render() {
-        document.querySelector('.score__number').textContent = String(this.totalScore);
-    }
-    static getInstance() {
-        if (!Score.instance) {
-            Score.instance = new Score();
-        }
-        return Score.instance;
-    }
-}
-class Food {
-    constructor(element) {
-        this.element = element;
-        element.addEventListener('click', this.clickEventHandler.bind(this));
-    }
-    clickEventHandler() {
-        this.element.classList.toggle('food--active');
-        const score = Score.getInstance();
-        score.render();
-    }
-}
 class Foods {
     constructor() {
         this.elements = document.querySelectorAll('.food');
@@ -61,4 +34,3 @@ class Foods {
         return Foods.instance;
     }
 }
-const foods = Foods.getInstance();
