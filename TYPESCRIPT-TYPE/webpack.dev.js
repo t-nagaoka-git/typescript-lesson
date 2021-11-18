0fb1baae4db0304975d7b983106ba310506379f3
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-11-19 08:52:23 +0900

    144. これが型定義ファイル(.d.ts)だ！

--- TYPESCRIPT-TYPE/webpack.dev.js
+++ TYPESCRIPT-TYPE/webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   mode: 'development',
-  entry: './src/food-app/main.ts',
+  entry: './src/library.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
--- TYPESCRIPT-TYPE/webpack.prod.js
+++ TYPESCRIPT-TYPE/webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
   mode: 'production',
-  entry: './src/food-app/main.ts',
+  entry: './src/library.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
