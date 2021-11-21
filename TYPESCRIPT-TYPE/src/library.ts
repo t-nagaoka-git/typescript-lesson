55c2f6585047247b937c22bc177e66fc44cbd858
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-11-21 09:17:33 +0900

    148. namespaceはこう使う！

--- TYPESCRIPT-TYPE/src/library.ts
+++ TYPESCRIPT-TYPE/src/library.ts
@@ -1,4 +1,9 @@
 // import axios from 'axios';
 // axios.get('https://fooapi.com');
 // import _ from 'lodash';
-console.log(_.shuffle([1, 2, 3, 4]));
+// console.log(_.shuffle([1, 2, 3, 4]));
+namespace myApp {
+  const hello = 'hello in namespace';
+  export const name = 'Quill';
+}
+const hello = myApp.name;
