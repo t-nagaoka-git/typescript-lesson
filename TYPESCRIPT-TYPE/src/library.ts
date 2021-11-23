aa67c408b79cf86f7f898e7aba2be29da6b06bd7
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-11-23 22:17:18 +0900

    151. lodashの型定義ファイルはこうなっている

--- TYPESCRIPT-TYPE/src/library.ts
+++ TYPESCRIPT-TYPE/src/library.ts
@@ -1,7 +1,7 @@
-// import axios from 'axios';
-// axios.get('https://fooapi.com');
-// import _ from 'lodash';
-// console.log(_.shuffle([1, 2, 3, 4]));
+import axios from 'axios';
+import _ from 'lodash';
+axios.get('https://fooapi.com');
+console.log(_.shuffle([1, 2, 3, 4]));
 namespace myApp {
   const hello = 'hello in namespace';
   export const name = 'Quill';
