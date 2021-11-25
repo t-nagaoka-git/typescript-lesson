2e967b3ab00379c33f60f97d9fdb597071b87c2c
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-11-25 21:34:10 +0900

    154. 型定義ファイルを拡張する方法

--- TYPESCRIPT-TYPE/src/lodash.d.ts
+++ TYPESCRIPT-TYPE/src/lodash.d.ts
@@ -1,7 +1,6 @@
-// declare module 'lodash' {
-//   export function shuffle<T>(arr: T[]): T[];
-// }
-// interface Loadash {
-//   shuffle<T>(arr: T[]): T[];
-// }
-// declare const _: Loadash;
+import _ from 'lodash';
+declare module 'lodash' {
+  interface LoDashStatic {
+    hello: string;
+  }
+}
