fd2f5439241f693b7b7997afb6045a3e4b356b54
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-12-01 10:59:57 +0900

    168. Expressを使ってルーティングの処理をする

--- TYPESCRIPT-NODE/dist/app.js
+++ TYPESCRIPT-NODE/dist/app.js
@@ -4,6 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import express = require('express');
 const app = (0, express_1.default)();
+app.use((req, res, next) => {
+    console.log('hello!');
+    next();
+});
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello</h1>');
+});
+// app.use((err, req, res, next) => {
+//   next();
+// });
 app.listen(3000);
