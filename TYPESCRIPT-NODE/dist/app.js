857dc005b59a5d6eda4ab83f843b2c924cf50e10
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-12-02 09:05:29 +0900

    170. 型を拡張してRequestのbodyに正しい型をつける方法

--- TYPESCRIPT-NODE/dist/app.js
+++ TYPESCRIPT-NODE/dist/app.js
@@ -5,12 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.use((req, res, next) => {
     console.log('hello!');
     next();
 });
 app.get('/', (req, res, next) => {
     res.send('<h1>Hello</h1>');
+});
+app.post('/', (req, res, next) => {
+    console.log(req.body);
+    res.send(`<h1>I got ${req.body.message}</h1>`);
 });
 app.use((err, req, res, next) => {
     next();
--- TYPESCRIPT-NODE/src/app.ts
+++ TYPESCRIPT-NODE/src/app.ts
@@ -1,11 +1,21 @@
 import express, {Request, Response, NextFunction} from 'express';
+interface MessageRequest extends Request {
+  body: {
+    message: string;
+  };
+}
 const app = express();
+app.use(express.json());
 app.use((req, res, next) => {
   console.log('hello!');
   next();
 });
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello</h1>');
+});
+app.post('/', (req: MessageRequest, res, next) => {
+  console.log(req.body);
+  res.send(`<h1>I got ${req.body.message}</h1>`);
 });
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   next();
